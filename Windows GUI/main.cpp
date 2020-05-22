e18db73d3ad93517e8199977dee6e6e32e52476d
yyt6801 <yyt6801@gmail.com>
2020-05-22 15:29:07 +0800

    add win32api gui

---
+++ Windows GUI/main.cpp
@@ -0,0 +1,44 @@
+#include <windows.h>
+//有问题，能运行但不出现窗体
+
+LRESULT CALLBACK WindowProcedure(HWND,UINT,WPARAM,LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInst , HINSTANCE hPrevInst, LPSTR args,int ncmdshow)
+{
+    //MessageBox(NULL,TEXT("hello world !!!"),TEXT("Note"),MB_OK);
+    WNDCLASSW wc = {0};
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL,IDC_ARROW);
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"myWindowsClass";
+    wc.lpfnWndProc = WindowProcedure;
+
+    if(!RegisterClassW(&wc))
+        return -1;
+
+    CreateWindowW(L"myWindowClass",L"My Window",WS_OVERLAPPEDWINDOW | WS_VISIBLE,100,100,500,500,
+                    NULL,NULL,NULL,NULL);
+    MSG msg = {0};
+
+    while(GetMessage(&msg,NULL,NULL,NULL))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+    
+
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd,UINT msg,WPARAM wp,LPARAM lp)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    
+    default:
+        return DefWindowProcW(hWnd,msg,wp,lp);
+    }
+}
