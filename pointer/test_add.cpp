1455ea5ff2f337772c365e3c934030297fdb579e
yyt6801 <yyt6801@gmail.com>
2020-06-23 00:00:15 +0800

    update pointer

--- pointer/test_add.cpp
+++ pointer/test_add.cpp
@@ -2,20 +2,17 @@
 
 int main()
 {
-    int a = 123;
-    int* p = &a;
-    int *m = &a;
-    printf("%x\n",p);//p的值
-    printf("%x\n",*p);
+    int a = 123;   //61ff1c
+
+    int *p = &a;//   p  61ff1c       *p=a错
+
+    printf("%x\n",p);//p的值     
+    printf("%d\n",*p);//          0x...   
     printf("%x\n",&p);//p的地址
     
     
-    printf("%x\n",m);//m的值
-    printf("%x\n",*m);
-    printf("%x\n",&m);//m的地址,新开辟的空间
-
-    printf("%x\n",a);//*p 代表的是 a 的值
-    printf("%x\n",&a);
+    printf("%d\n",a);//*p 代表的是 a 的值  123
+    printf("%x\n",&a);//          0x...
     
 
 }
