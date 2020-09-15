d8a3f9bccfcc8c38f5b0ed9d2450a5d0abd1cbb8
yyt6801 <yyt6801@gmail.com>
2020-09-15 14:03:17 +0800

    update 多条件判断

--- 001/print_hello_world.cpp
+++ 001/print_hello_world.cpp
@@ -1,14 +1,14 @@
-//绠�鍗曟祴璇曟墦鍗板嚭Hello_world!!!
+//简单测试打印出Hello_world!!!
 #include <stdio.h>
 #include <iostream>
 using namespace std;
 int main()
 {
-	
-// enum days {SUN = 1, MON, TUE, WED, THU, FRI, SAT};
-// enum days d = SUN;
-	float C40_PASSRATE =0;
-	float W40_PASSRATE=0;
-	float new_probability,probability=0.5;
-	(C40_PASSRATE == 0 && W40_PASSRATE == 0) ? printf("鍑稿害妤斿舰鍛戒腑鐜囬兘涓洪浂璁や负璇ュ嵎閽㈡棤鍑稿害妤斿舰") : ({ new_probability = probability + (1 - probability) * 0.5 * ((1 - C40_PASSRATE) + (1 - W40_PASSRATE));printf("鏂拌绠楄窇鍋忔鐜囦负%f", new_probability); });
+
+	// enum days {SUN = 1, MON, TUE, WED, THU, FRI, SAT};
+	// enum days d = SUN;
+	float C40_PASSRATE = 0;
+	float W40_PASSRATE = 0;
+	float new_probability, probability = 0.5;
+	(C40_PASSRATE == 0 && W40_PASSRATE == 0) ? printf("凸度楔形命中率都为零认为该卷钢无凸度楔形") : ({ new_probability = probability + (1 - probability) * 0.5 * ((1 - C40_PASSRATE) + (1 - W40_PASSRATE));printf("新计算跑偏概率为%f", new_probability); });
 }
---
+++ 多条件判断/multi_if.cpp
@@ -0,0 +1,17 @@
+//ºÚµ•≤‚ ‘¥Ú”°≥ˆHello_world!!!
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 15;
+    int a_0 = 2;
+    if ((a == 15 || a == 16) && a_0 == 2)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("none");
+    }
+}
