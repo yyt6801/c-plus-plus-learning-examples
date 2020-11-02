57de25443f7f972507c58fc8799168e2636934f9
yyt6801 <yyt6801@gmail.com>
2020-11-02 17:43:34 +0800

    添加单链表插入排序

---
+++ 直接插入排序/danlianbiao_charupaixu.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <cstdlib> //malloc需要的标准库
+//单链表定义
+typedef struct list
+{
+	int data;
+	struct list * next;
+}list,*linklist;
+
+//单链表的创建
+void create(linklist &L,int n)
+{
+	int i;
+	linklist p;
+	L = (linklist)malloc(sizeof(list));
+	L->next = NULL;
+	for(i=0;i<n;i++)
+	{
+		p = (linklist)malloc(sizeof(list));
+		scanf("%d",&p->data);
+		p->next = L->next;
+		L->next = p;
+	}
+}
+
+//单链表输出
+void show(linklist L)
+{
+	linklist p;
+	p = L->next;
+	while(p)
+	{
+		printf("%d\t",p->data);
+		p = p->next;
+	}
+}
+
+//直接插入排序
+void insertsort(linklist L)
+{
+	linklist p,q,pre;
+	p = L->next->next;//p为L的第二个结点
+	L->next->next = NULL;//把L置为单结点的链表
+	while(p)
+	{
+		q = p->next;//p的下一个结点
+		pre = L;
+		while(pre->next != NULL && pre->next->data < p->data)
+			pre = pre->next;
+		p->next = pre->next;
+		pre->next = p;
+		p = q;
+	}
+}
+int main(){
+    linklist   L;
+    create(L,5);
+    show(L);
+    insertsort(L);
+    show(L);
+}
--- 直接插入排序/zhijiecharupaixu.cpp
+++ 直接插入排序/zhijiecharupaixu.cpp
@@ -3,10 +3,11 @@ int main()
 {
     int Length = 10;                                  //需要排序的序列arr的长度
     int arr[Length] = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0}; //这是需要排序的序列
+    int temp;
     //第一个数为有序，其他数为无序
     for (int i = 1; i < Length; i++)
     {
-        int temp = arr[i];
+        temp= arr[i];
         //依次和有序数做比较
         for (int j = i; j > 0; j--)
         {
