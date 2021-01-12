9af9faec8653742ea2adcccbc466864fc7cd58f7
yyt6801 <yyt6801@gmail.com>
2021-01-12 11:00:52 +0800

    添加时间格式字符串之间相互转化

--- 001/test.cpp
+++ 001/test.cpp
@@ -10,5 +10,15 @@ int main()
     string myGraphJson = "{\"kind\":{\"graph\":true},"
         "\"nodes\":[{\"id\":\"1\"},{\"id\":\"2\"}],"
         "\"edges\":[{\"from\":\"1\",\"to\":\"2\"}]}";
-    cout << myGraphJson;
+    cout << myGraphJson <<endl;
+
+    
+    string aaa;
+    aaa = "H123456711890";
+    if (aaa[8]!='0')
+    {
+        printf("%c\n",aaa[8]);
+    }
+    
+    printf("end%s",aaa);
 }
