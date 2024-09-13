b05555b79da04d84ff8bd1da105ca4f6d8662ed4
Kevin Yuan <1725056801@qq.com>
2024-09-13 16:54:16 +0800

    update mqtt

--- MQTT/发送MQTT消息 -ok.cpp
+++ MQTT/发送MQTT消息 -ok.cpp
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "MQTTClient.h"
 #include <mqtt/client.h>
 #pragma warning( disable : 4996 )
 
-#define ADDRESS     "tcp://127.0.0.1:1883"
-#define CLIENTID    "PCDP_Client"
+#define ADDRESS     "tcp://192.168.0.103:1883"
+#define CLIENTID    "PCDP_Client4"
+#define TOPIC		"chat"
+#define USERNAME	"admin"
+#define PASSWORD	"password"
 #define QOS         1
 #define TIMEOUT     10000L
 
 int main(int argc, char* argv[])
 {
-	const std::string TOPIC = "PLTCM/TAGS";
-
 	// 创建客户端
 	mqtt::client cli(ADDRESS, CLIENTID);
 	mqtt::connect_options connOpts;
 	connOpts.set_keep_alive_interval(20);
+	connOpts.set_user_name(USERNAME);
+	connOpts.set_password(PASSWORD);
 	connOpts.set_clean_session(true);
 
 	try {
@@ -26,15 +28,23 @@ int main(int argc, char* argv[])
 
 		// Keep the client running to receive messages
 		while (true) {
-			std::this_thread::sleep_for(std::chrono::seconds(1));
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
 			// 创建并发布消息
-			time_t now = time(NULL);
-			tm* time_tm = localtime(&now);
+			//time_t now = time(NULL);
+			//tm* time_tm = localtime(&now);
+			//通过不同精度获取相差的毫秒数
+			auto now = std::chrono::system_clock::now(); 
+			time_t tt = std::chrono::system_clock::to_time_t(now);
+			auto time_tm = localtime(&tt);
+
+			uint64_t dis_millseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count()
+				- std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count() * 1000;
+
 			char strTime[25] = { 0 };
-			sprintf(strTime, "%d-%02d-%02d %02d:%02d:%02d", time_tm->tm_year + 1900,
+			sprintf(strTime, "%d-%02d-%02d %02d:%02d:%02d:%03d", time_tm->tm_year + 1900,
 				time_tm->tm_mon + 1, time_tm->tm_mday, time_tm->tm_hour,
-				time_tm->tm_min, time_tm->tm_sec);
+				time_tm->tm_min, time_tm->tm_sec, (int)dis_millseconds);
 			std::cout << strTime << std::endl;
 
 			const std::string PAYLOAD = strTime;
